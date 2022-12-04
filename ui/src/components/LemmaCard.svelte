ca00ba0df36de37967aac91708ced06415dc6be6
Darkclainer <Darkclainer@gmail.com>
2022-12-05 01:34:35 +0600

    ui: add simple mobile version

--- ui/src/components/LemmaCard.svelte
+++ ui/src/components/LemmaCard.svelte
@@ -24,7 +24,9 @@
 	};
 </script>
 
-<div class="flex justify-between shadow-md rounded-md bg-gray my-4 px-8 pt-10 pb-7">
+<div
+	class="flex flex-col sm:flex-row justify-between shadow-md rounded-md bg-gray my-4 px-8 pt-10 pb-7"
+>
 	<div class="grow divide-y divide-blue flex-1">
 		<div>
 			<button
@@ -91,13 +93,13 @@
 			</div>
 		{/if}
 	</div>
-	<div class="shrink basis-14" />
-	<div class="flex-none flex flex-col justify-items-stretch basis-40">
+	<div class="shrink sm:basis-8 md:basis-14" />
+	<div class="flex-none flex flex-col-reverse sm:flex-col justify-items-stretch basis-40">
 		<button
 			class="px-2 py-4 text-xl rounded-md bg-blue hover:bg-green active:bg-dark-green transition-colors duration-300 text-white"
 			>Add to Anki</button
 		>
-		<div class="divide-y divide-blue">
+		<div class="flex flex-col-reverse sm:flex-col sm:divide-y divide-blue">
 			{#if lemma.tags.length != 0}
 				<div class="flex flex-col justify-items-center gap-4 py-4 text-lg text-blue">
 					{#each lemma.tags as tag}
@@ -110,7 +112,7 @@
 					<button class="m-2" on:mouseup={play_mouse_up}>
 						<PlayIcon />
 					</button>
-					<p class="m-1 text-dark-gray text-sm leading-5">Listen to pronunciation</p>
+					<p class="m-1 text-dark-gray my-auto text-sm leading-5">Listen to pronunciation</p>
 					<audio bind:this={audio}>
 						{#each lemma.audio as audio}
 							<source src={audio.source} type={audio.type} />
