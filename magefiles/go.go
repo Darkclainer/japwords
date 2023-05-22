865e155a0525323c3869669fe62f55ded4419ff5
Darkclainer <Darkclainer@gmail.com>
2023-05-22 20:05:36 +0400

    Add go mage targets: go:lint, go:generate

---
+++ magefiles/go.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"path/filepath"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+type Go mg.Namespace
+
+func (Go) Lint(ctx context.Context) error {
+	mg.CtxDeps(ctx, mg.F(Tools.Install, "golangci-lint"))
+	golangci := getToolPath("golangci-lint")
+	return sh.RunV(golangci, "run", "--sort-results")
+}
+
+func (Go) Generate(ctx context.Context) error {
+	mg.CtxDeps(ctx, mg.F(Tools.Install, "mockery"))
+	mockery, err := filepath.Abs(getToolPath("mockery"))
+	if err != nil {
+		return err
+	}
+	return sh.RunWithV(
+		map[string]string{
+			"MOCKERY_TOOL": mockery,
+		},
+		"go", "generate", "./...",
+	)
+}
