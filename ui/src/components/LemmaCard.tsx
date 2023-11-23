e428b696edb6d7568cff9527fcf9ce6fb7053695
Darkclainer <Darkclainer@gmail.com>
2023-11-23 15:07:00 +0400

    fix(ui): add proper scaffolding for icons, add PlayIcon

--- ui/src/components/LemmaCard.tsx
+++ ui/src/components/LemmaCard.tsx
@@ -4,6 +4,7 @@ import { useRef } from 'react';
 import { Audio, Furigana, Lemma, PitchType, Sense, Word } from '../api/__generated__/graphql';
 import { ToastFunction } from '../lib/styled-toast';
 import Button from './Button';
+import PlayIcon from './Icons/PlayIcon';
 
 function RenderFurigana(props: { furigana: Furigana }) {
   const { furigana } = props;
@@ -178,20 +179,7 @@ function AudioControls(props: { audios: Audio[] }) {
           handleMouseUp();
         }}
       >
-        <svg
-          className="fill-blue hover:fill-green active:fill-dark-green transition-color transition-colors duration-300"
-          width="40"
-          height="40"
-          viewBox="10 10 41 41"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="31" cy="31" r="20" />
-          <path
-            d="M26.5904 21.7116C26.7411 21.6368 26.9118 21.6579 27.0393 21.7559L41.0688 30.7172C41.1679 30.7994 41.2311 30.9185 41.2311 31.0471C41.2311 31.1735 41.1669 31.2968 41.0688 31.378L27.0383 40.3382C26.9645 40.3983 26.8675 40.4299 26.7748 40.4299L26.5893 40.3878C26.446 40.3193 26.3553 40.1696 26.3553 40.0073V22.09C26.3553 21.9298 26.446 21.7791 26.5904 21.7116Z"
-            fill="white"
-          />
-        </svg>
+        <PlayIcon className="h-10 w-10 fill-blue hover:fill-green active:fill-dark-green transition-color transition-colors duration-300" />
       </button>
       <p className="m-1 text-dark-gray my-auto text-lg leading-5">Listen to pronunciation</p>
       {/* eslint-disable-next-line jsx-a11y/media-has-caption
